db97ee3056400330e2c28a9b7d4e257e4f41cdea
venkat <ramanavenkata322@gmail.com>
2018-07-19 11:51:47 +0530

    Add users listing and update show article view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url,alt: user.username, class: "image_square")
+    image_tag(gravatar_url,alt: user.username, class: "image_circle")
 
 
    end
