d4cffa4ecb06f475db4ea811a782977febf43921
venkat <ramanavenkata322@gmail.com>
2018-07-26 11:41:05 +0530

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ get 'about',to:'pages#about'
 resources :articles
 get 'signup', to: 'users#new'
 resources :users, except: [:new]
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
 
 end
