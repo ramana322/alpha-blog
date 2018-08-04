55e3a9ddf10d6f3bacfb91595208002bf5f8525e
venkat <ramanavenkata322@gmail.com>
2018-08-04 12:14:31 +0530

    added admint to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_054427) do
+ActiveRecord::Schema.define(version: 2018_08_04_064225) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
