ab4cd73c6f70f9db7f072f7431ac50a73d9bd6b1
venkat <ramanavenkata322@gmail.com>
2018-08-08 12:19:54 +0530

    updated

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+
+	def index
+		@books = Book.all
+	end
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+
+		if @book.save
+			redirect_to books_path
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		@book = Book.find(params[:id])
+
+		if @book.update(book_params)
+			redirect_to books_path
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def book_params
+		params.require(:book).permit(:name, :author, :price)
+	end
+
+end
